587f658f0ca9e285a24cf6011349c60d0fa99039
Jordan anderson <jordan_anders@live.co.uk>
2018-06-17 13:39:59 +0100

    Adds support for dependency injection

--- src/Anderson.Pipelines/Builders/PipelineDefinitionBuilder.cs
+++ src/Anderson.Pipelines/Builders/PipelineDefinitionBuilder.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using Anderson.Pipelines.Definitions;
 using Anderson.Pipelines.Handlers;
 
@@ -11,6 +12,19 @@ namespace Anderson.Pipelines.Builders
     /// <typeparam name="TResponse">The response which the pipeline produces</typeparam>
     public class PipelineDefinitionBuilder<TRoot, TResponse>
     {
+        private readonly Func<Type, object> _resolveService;
+
+        /// <summary>
+        /// Uses DI container to resolve pipes and dependancies by providing
+        /// the function to resolve services
+        /// </summary>
+        /// <param name="resolveService"></param>
+        /// <returns></returns>
+        public PipelineDefinitionBuilder(Func<Type, object> resolveService)
+        {
+            _resolveService = resolveService;
+        }
+
         /// <summary>
         /// The initial node that begins the pipeline
         /// </summary>
@@ -24,6 +38,20 @@ namespace Anderson.Pipelines.Builders
                 _root = rootDefinition
             };
         }
+
+        /// <summary>
+        /// The initial node that begins the pipeline
+        /// </summary>
+        /// <returns></returns>
+        public PipelineDefinitionBuilder<TRoot, TRoot, TResponse> StartWith<T>() where T : PipelineDefinition<TRoot, TResponse>
+        {
+            PipelineDefinition<TRoot, TResponse> pipe = (PipelineDefinition<TRoot, TResponse>) _resolveService(typeof(T));
+
+            return new PipelineDefinitionBuilder<TRoot, TRoot, TResponse>(pipe, _resolveService)
+            {
+                _root = pipe
+            };
+        }
     }
 
     /// <summary>
@@ -34,35 +62,57 @@ namespace Anderson.Pipelines.Builders
     /// <typeparam name="TResponse">The response which the pipeline produces</typeparam>
     public class PipelineDefinitionBuilder<TRoot, TChild, TResponse>
     {
+        private readonly Func<Type, object> _resolveService;
         internal IInnerHandler<TRoot, TResponse> _root;
         private readonly IInnerHandler<TChild, TResponse> _current;
 
         internal PipelineDefinitionBuilder(
             IInnerHandler<TRoot, TResponse> rootDefinition,
-            IInnerHandler<TChild, TResponse> currentDefinition)
+            IInnerHandler<TChild, TResponse> currentDefinition,
+            Func<Type, object> resolveService)
         {
             _root = rootDefinition;
             _current = currentDefinition;
+            _resolveService = resolveService;
         }
 
-        internal PipelineDefinitionBuilder(
-            PipelineDefinition<TChild, TResponse> currentDefinition)
+        internal PipelineDefinitionBuilder(PipelineDefinition<TChild, TResponse> currentDefinition)
         {
             _current = currentDefinition;
+        }
+
+        internal PipelineDefinitionBuilder(PipelineDefinition<TChild, TResponse> currentDefinition, Func<Type, object> resolveService) : this(currentDefinition)
+        {
+            _resolveService = resolveService;
         }
 
         /// <summary>
         /// Adds the next node into the pipeline. Pipelines execute in the order nodes are added
         /// </summary>
-        /// <param name="pipelineDefinition">Defines how requests are handled</param>
+        /// <param name="pipe">Defines how requests are handled</param>
         /// <returns></returns>
         public PipelineDefinitionBuilder<TRoot, TChild, TResponse> ThenWith(
-            PipelineDefinition<TChild, TResponse> pipelineDefinition)
+            PipelineDefinition<TChild, TResponse> pipe)
         {
-            _current.InnerHandler = pipelineDefinition;
+            _current.InnerHandler = pipe;
             return new PipelineDefinitionBuilder<TRoot, TChild, TResponse>(
                 _root, 
-                pipelineDefinition);
+                pipe,
+                _resolveService);
+        }
+
+        /// <summary>
+        /// Adds the next node into the pipeline. Pipelines execute in the order nodes are added
+        /// </summary>
+        /// <returns></returns>
+        public PipelineDefinitionBuilder<TRoot, TChild, TResponse> ThenWith<T>() where T : PipelineDefinition<TChild, TResponse>
+        {
+            PipelineDefinition<TChild, TResponse> pipe = (PipelineDefinition<TChild, TResponse>)_resolveService(typeof(T));
+            _current.InnerHandler = pipe;
+            return new PipelineDefinitionBuilder<TRoot, TChild, TResponse>(
+                _root,
+                pipe,
+                _resolveService);
         }
 
         /// <summary>
@@ -70,15 +120,34 @@ namespace Anderson.Pipelines.Builders
         /// Further nodes in the pipeline from this point will now handle <typeparam name="TMutatedRequest"></typeparam>
         /// </summary>
         /// <typeparam name="TMutatedRequest"></typeparam>
-        /// <param name="pipelineDefinition"></param>
+        /// <param name="pipe"></param>
         /// <returns></returns>
         public PipelineDefinitionBuilder<TRoot, TMutatedRequest, TResponse> ThenWithMutation<TMutatedRequest>(
-            PipelineMutationDefinition<TChild, TMutatedRequest, TResponse> pipelineDefinition)
+            PipelineMutationDefinition<TChild, TMutatedRequest, TResponse> pipe)
         {
-            _current.InnerHandler = pipelineDefinition;
+            _current.InnerHandler = pipe;
             return new PipelineDefinitionBuilder<TRoot, TMutatedRequest, TResponse>(
                 _root, 
-                pipelineDefinition);
+                pipe,
+                _resolveService);
+        }
+
+        /// <summary>
+        /// Changes the Initial request into a different type.
+        /// Further nodes in the pipeline from this point will now handle <typeparam name="TMutatedRequest"></typeparam>
+        /// </summary>
+        /// <typeparam name="TMutatedRequest"></typeparam>
+        /// <typeparam name="TPipe"></typeparam>
+        /// <returns></returns>
+        public PipelineDefinitionBuilder<TRoot, TMutatedRequest, TResponse> ThenWithMutation<TPipe, TMutatedRequest>() 
+            where TPipe : PipelineMutationDefinition<TChild, TMutatedRequest, TResponse>
+        {
+            PipelineMutationDefinition<TChild, TMutatedRequest, TResponse> pipe = (PipelineMutationDefinition<TChild, TMutatedRequest, TResponse>)_resolveService(typeof(TPipe));
+            _current.InnerHandler = pipe;
+            return new PipelineDefinitionBuilder<TRoot, TMutatedRequest, TResponse>(
+                _root,
+                pipe,
+                _resolveService);
         }
 
         /// <summary>
